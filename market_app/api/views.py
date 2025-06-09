014523d612f3f42fd3bc0a6fb37c218059d33055
Leugzim Rullani <leugzimrullani@outlook.com>
2025-06-09 20:40:41 +0200

    token begin

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from market_app.models import Manufacturer, Product, ManufacturerUser
 from .serializers import ManufacturerSerializer, ProductSerializer, ManufacturerUserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from .permissions import IsStaffOrReadOnly, IsAdminForDeleteOrPatchAndReadOnly  # später hinzufügen! IsAuthenticatedOrReadOnly  
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated 
+from .permissions import IsStaffOrReadOnly, IsAdminForDeleteOrPatchAndReadOnly, IsOwnerOrAdmin  # später hinzufügen! IsAuthenticatedOrReadOnly  
 
 class ManufacturerList(generics.ListCreateAPIView):
     queryset = Manufacturer.objects.all()
@@ -32,7 +32,7 @@ class ManufacturerUserList(generics.ListCreateAPIView):
 class ManufacturerUserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ManufacturerUser.objects.all()
     serializer_class = ManufacturerUserSerializer
-    #permission_classes = [IsOwnerOrAdmin]                           # später hinzufügen!
+    permission_classes = [IsOwnerOrAdmin]                           # später hinzufügen!
 
 
 class ManufacturerProductListCreate(generics.ListCreateAPIView):
